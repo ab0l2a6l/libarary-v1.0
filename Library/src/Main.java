8c61b2c625db2ac42e647c5274a5e1954d352302
eta2j <hamid.2jahandideh@gmail.com>
2023-05-25 18:57:04 +0330

    say kardam ba Enter beyne code ha ghesmat bandi anjam bedam

--- Library/src/Main.java
+++ Library/src/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        //mamoolan intori nist. mamoolan har variable joda tarif mishe:
         int num;
         int temp;
 
